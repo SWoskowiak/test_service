705ab7d1003fb821e23f74bfd01924e5393d01ab
Stefan Woskowiak <swoskowiak@gmail.com>
2018-09-16 13:34:19 -0700

    route added to next call in middleware

--- routes/medical_id.js
+++ routes/medical_id.js
@@ -21,7 +21,7 @@ router.use('/v1/medical_id/:user_id', async (req, res, next) => {
   let medicalID = await MedicalID.fetch(userID)
 
   res.locals.medicalID = medicalID
-  next()
+  next('route')
 })
 
 // Get medical_id information for a user
