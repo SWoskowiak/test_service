2bd1744b20215399a3f1a97980a720c3273726fc
Stefan Woskowiak <swoskowiak@gmail.com>
2018-09-16 16:42:50 -0700

    quick commit

--- routes/medical_id.js
+++ routes/medical_id.js
@@ -15,16 +15,24 @@ const MedicalID = require('../resources/medical_id')
 // upload state id and meta-data, view data, update or delete the ID/Rec
 
 // Basic middleware that adds a medical_id resource to res.locals
-module.context.use(async (req, res, next) => {
+// module.context.use(async (req, res, next) => {
+//   const userID = req.pathParams.user_id
+//   res.locals = res.locals || {}
+
+//   res.locals.medicalID = await MedicalID.fetch(userID)
+//   return next()
+// })
+
+async function medicalMiddleware (req, res, next) {
   const userID = req.pathParams.user_id
   res.locals = res.locals || {}
 
   res.locals.medicalID = await MedicalID.fetch(userID)
   return next()
-})
+}
 
 // Get medical_id information for a user
-router.get('/v1/medical_id/:user_id', (req, res) => {
+router.get('/v1/medical_id/:user_id', medicalMiddleware, (req, res) => {
   res.send(`Hello ${req.pathParams.user_id}!, your medical id# is: ${res.locals.medicalID} `)
 })
   .response(['text/plain'], 'A generic greeting.')
