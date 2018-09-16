e63d4201903378be7e141ec2b7ddbacb331c8f21
Stefan Woskowiak <swoskowiak@gmail.com>
2018-09-16 16:30:59 -0700

    quick commit

--- routes/medical_id.js
+++ routes/medical_id.js
@@ -15,11 +15,11 @@ const MedicalID = require('../resources/medical_id')
 // upload state id and meta-data, view data, update or delete the ID/Rec
 
 // Basic middleware that adds a medical_id resource to res.locals
-module.context.use(async (req, res, next) => {
+module.context.use((req, res, next) => {
   const userID = req.pathParams.user_id
   res.locals = res.locals || {}
 
-  res.locals.medicalID = await MedicalID.fetch(userID)
+  res.locals.medicalID = MedicalID.fetch(userID)
   next()
 })
 
